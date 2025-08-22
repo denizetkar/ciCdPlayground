aadd8600350b0a8862eb7926b918834c6eb96087
Deniz Etkar <deniz.etkar@tngtech.com>
2025-08-22 13:26:44 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Deniz Etkar",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Github Actions", "jenkins", "testing", "deployment"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Github Actions",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: [
+      "Other popular CI/CD tools",
+      "How to setup CI/CD for a greenfield project from scratch (no existing infra guidance)?",
+    ],
   },
 });
